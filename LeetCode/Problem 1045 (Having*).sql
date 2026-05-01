7331043c54ae85d8ba73023a69fed565e6885e72
Iftaker Ahmed Tahmid <ahmedeftakhertahmid@gmail.com>
2026-05-01 17:30:36 +0600

    Create SQL query for customers buying all products
    Add SQL query to find customers who bought all products

---
+++ LeetCode/Problem 1045 (Having*).sql
@@ -0,0 +1,7 @@
+--Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+--Customers who bought all product
+SELECT customer_id
+FROM Customer 
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
+;
